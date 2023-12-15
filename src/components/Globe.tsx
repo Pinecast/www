888ec140ae3d0623f979801500fac8a1afb1b8f4
Christopher Van <203725+cvan@users.noreply.github.com>
2023-12-14 17:01:32 -0800

    remove unnecessary SVG dimensions, also resolve console warnings

--- src/components/Globe.tsx
+++ src/components/Globe.tsx
@@ -87,8 +87,6 @@ function GlobeDesktop({
   return (
     <svg
       fill="none"
-      width="100%"
-      height="auto"
       viewBox="0 0 1670 835"
       xmlns="http://www.w3.org/2000/svg"
       style={
