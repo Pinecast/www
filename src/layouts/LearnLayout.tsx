cc0a3d9a413703b4d23b6aab475644d3df56a881
Matt Basta <mattbasta+github@gmail.com>
2023-12-31 10:12:52 -0500

    Fix missing color

--- src/layouts/LearnLayout.tsx
+++ src/layouts/LearnLayout.tsx
@@ -2,11 +2,14 @@ import {H1, PillButton} from '@/components/Typography';
 import * as React from 'react';
 import {BaseLayout} from './BaseLayout';
 
-export const LearnLayout = BaseLayout(({title}) => (
-  <>
-    <PillButton style={{textTransform: 'uppercase', marginBottom: '30px'}}>
-      Learn with Pinecast
-    </PillButton>
-    <H1 style={{padding: '0 10%', marginBottom: '50px'}}>{title}</H1>
-  </>
-));
+export const LearnLayout = BaseLayout(
+  ({title}) => (
+    <>
+      <PillButton style={{textTransform: 'uppercase', marginBottom: '30px'}}>
+        Learn with Pinecast
+      </PillButton>
+      <H1 style={{padding: '0 10%', marginBottom: '50px'}}>{title}</H1>
+    </>
+  ),
+  {defaultColor: 'var(--color-lime)'},
+);
