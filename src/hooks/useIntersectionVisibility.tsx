91d3c3ac6d28ce6431a011333357bce6bfdfaadb
Christopher Van <203725+cvan@users.noreply.github.com>
2024-01-06 14:57:51 -0800

    fix bug with useIntersectionVisibility with callback fired for other global target elements with the reused observer

--- src/hooks/useIntersectionVisibility.tsx
+++ src/hooks/useIntersectionVisibility.tsx
@@ -1,33 +1,33 @@
 import * as React from 'react';
 
-const elementMap = new Map<HTMLElement, (isIntersecting: boolean) => void>();
-
-const observer =
-  typeof IntersectionObserver !== 'undefined'
-    ? new IntersectionObserver(entries => {
-        entries.forEach(({target, isIntersecting}) => {
-          elementMap.get(target as HTMLElement)?.(isIntersecting);
-        });
-      })
-    : undefined;
-
 export const useIntersectionVisibility = (
   ref: React.RefObject<HTMLElement>,
   callback: (isIntersecting: boolean) => void,
 ) => {
+  const elementMapRef = React.useRef(new Map<HTMLElement, (isIntersecting: boolean) => void>());
   React.useEffect(() => {
     const element = ref.current;
     if (!element) {
       return;
     }
+    const elementMap = elementMapRef.current;
     if (elementMap.has(element)) {
       throw new Error('Cannot observe the same element twice');
     }
     elementMap.set(element, callback);
+    const observer =
+      typeof IntersectionObserver !== 'undefined'
+        ? new IntersectionObserver(entries => {
+            entries.forEach(({target, isIntersecting}) => {
+              elementMap.get(target as HTMLElement)?.(isIntersecting);
+            });
+          })
+        : undefined;
     observer?.observe(element);
     return () => {
       elementMap.delete(element);
       observer?.unobserve(element);
+      observer?.disconnect();
     };
   }, [ref, callback]);
 };
