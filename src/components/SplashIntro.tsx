ed950583ec5dd2a67c8428ecd9f2941fd66274d6
Christopher Van <203725+cvan@users.noreply.github.com>
2024-02-27 21:08:13 -0800

    improve splash intro: hide background to avoid FOUC while lottie loads

--- src/components/SplashIntro.tsx
+++ src/components/SplashIntro.tsx
@@ -1,23 +1,36 @@
 import Lottie from 'lottie-react';
 import * as splash from '@/animations/splash.json';
+import {useCSS} from '@/hooks/useCSS';
 
 export const SplashIntro = ({onComplete}: {onComplete: () => void}) => {
+  const css = useCSS();
   return (
-    <Lottie
-      animationData={splash}
-      loop={false}
-      onComplete={onComplete}
-      aria-hidden="true"
-      className="splash-wrapper"
-      rendererSettings={{preserveAspectRatio: 'xMidYMid slice'}}
-      style={{
+    <div
+      className={css({
+        backgroundColor: '#000',
         position: 'fixed',
         top: '0',
-        left: '0',
         right: '0',
         bottom: '0',
+        left: '0',
         zIndex: 160,
-      }}
-    />
+      })}
+    >
+      <Lottie
+        animationData={splash}
+        loop={false}
+        onComplete={onComplete}
+        aria-hidden="true"
+        className="splash-wrapper"
+        rendererSettings={{preserveAspectRatio: 'xMidYMid slice'}}
+        style={{
+          position: 'absolute',
+          top: '0',
+          right: '0',
+          bottom: '0',
+          left: '0',
+        }}
+      />
+    </div>
   );
 };
